c96202fb15884e6de1e4e7bd7ccb14f24d48be9e
Maplewing <sinmaplewing@gmail.com>
2013-12-22 12:54:40 +0800

    Fix some component's event bugs.

--- source/knight.component.js
+++ source/knight.component.js
@@ -7,28 +7,26 @@ Knight.Component = (function(){
 	Knight.Utility.extend(Component, Knight.Event);
 
 	var _afterAdd = function(event, listener){
-		if(event === 'update'){
-			this._game.addEventListener('update', listener);
-		}
-		else if(event === 'draw'){
-			this._game.addEventListener('draw', listener);
+		if(event === 'update' || event === 'draw'){
+			this._game.addEventListener(event, listener);
 		}
 	};
 
 	var _afterRemove = function(event, listener){
-		if(event === 'update'){
-			this._game.removeEventListener('update', listener);
-		}
-		else if(event === 'draw'){
-			this._game.removeEventListener('draw', listener);
+		if(event === 'update' || event === 'draw'){
+			this._game.removeEventListener(event, listener);
 		}
 	};
 
 	Component.prototype.add = function(){
 		if( this._update )
-			this._game.addEventListener.apply(this._game, 'update', this._update);
+			this._update.forEach( function(listener){
+				this._game.addEventListener('update', listener);
+			}.bind(this));
 		if( this._draw )
-			this._game.addEventListener.apply(this._game, 'draw', this._draw);
+			this._draw.forEach( function(listener){
+				this._game.addEventListener('draw', listener);
+			}.bind(this));
 		this.addEventListener('addeventlistener', _afterAdd);
 		this.addEventListener('removeeventlistener', _afterRemove);
 		this.contain = true;
@@ -36,9 +34,13 @@ Knight.Component = (function(){
 
 	Component.prototype.remove = function(){
 		if( this._update )
-			this._game.removeEventListener.apply(this._game, 'update', this._update);
+			this._update.forEach( function(listener){
+				this._game.removeEventListener('update', listener);
+			}.bind(this));
 		if( this._draw )
-			this._game.removeEventListener.apply(this._game, 'draw', this._draw);
+			this._draw.forEach( function(listener){
+				this._game.removeEventListener('draw', listener);
+			}.bind(this));
 		this.removeEventListener('addeventlistener', _afterAdd);
 		this.removeEventListener('removeeventlistener', _afterRemove);
 		this.contain = false;
